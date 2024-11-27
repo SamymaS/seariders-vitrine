e8d858d5ef4debcf53ce8a19bdd5c70bcdc43ff8
Samy <samyboudaoud95@gmail.com>
2024-11-27 14:25:00 +0100

    Création Appjsx, architecture app vitrine + mise en place de tailwindcss et test navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './main.css';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-center mt-8">Bienvenue sur Sea Riders Vitrine</h1>
+    </div>
+    
+
+
+  );
+}
+
+
+export default App;
