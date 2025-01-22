66808b6381f467f89f827d87bfcb0f5df3fddea3
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2025-01-22 14:10:02 +0100

    Update La Page Products
    Page Products

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './css/main.css'; // Styles globaux
 import './css/Boutique.css'; // Style pour la Page Boutique
 import './css/Home.css'; // Style pour la Page Home
+import './css/Products.css'; // Style pour la Page Products
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
